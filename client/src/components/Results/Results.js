3a12c05628437f95f5f5c4d1b476e58f6cbbfc9f
Ross Blair <rblair33@gmail.com>
2017-11-05 14:46:11 -0500

    Loading different pages through React router. Style/content missing

---
+++ client/src/components/Results/Results.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Results = () => (
+    <div>Results is loading!</div>
+);
+
+export default Results;
---
+++ client/src/pages/Search/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Results from "../../components/Results";
+
+const Search = () => (
+    <div>
+        <div>Search is loading!</div>
+        <Results />
+    </div>
+);
+
+export default Search;
